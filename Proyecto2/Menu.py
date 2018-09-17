3dcc242f0e11f0067fa7043aad4847c4a52e91bc
Yocoyani Pérez <yocoyaniperez1@gmail.com>
2018-09-17 08:26:31 -0500

    Create Menu.py

---
+++ Proyecto2/Menu.py
@@ -0,0 +1,27 @@
+import Base
+import Empresas
+import Funciones
+
+class Menu():
+	
+	def __correr__(self):
+		
+		while True:
+			base.mostrarClases()
+			clase = input("Nombre de la clase: ", end = '')
+			seleccion = validacion("1)Editar objeto\n 2)Eliminar objeto\n3)Buscar objeto\n4)AgregarObjeto")
+			if seleccion == 1:
+				editarObjeto()
+				continue
+			if seleccion == 2:
+				elminarObjeto()
+				continue
+			if seleccion == 3:
+				buscarObjeto()
+				continue
+			if seleccion == 4:
+				agregarObjeto()
+				continue
+
+	def __init__(self):
+		self.__correr__()
